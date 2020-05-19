6acade2d9813c73ba44884564420982ba5a16799
PuneetAsnani12 <asnani.puneet@gmail.com>
2020-05-20 00:10:20 +0530

    added login form and reactive contact us form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { MatListModule } from "@angular/material/list";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
-
+import { MatDialogModule } from "@angular/material/dialog";
 import { AppRoutingModule } from "./app-routing.module";
-
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatSelectModule } from "@angular/material/select";
 import { AppComponent } from "./app.component";
-
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import "hammerjs";
 import { MenuComponent } from "./menu/menu.component";
 import { DishDetailComponent } from "./dish-detail/dish-detail.component";
@@ -23,6 +27,7 @@ import { ContactComponent } from "./contact/contact.component";
 import { DishService } from "./services/dish.service";
 import { PromotionService } from "./services/promotion.service";
 import { LeaderService } from "./services/leader.service";
+import { LoginComponent } from "./login/login.component";
 
 @NgModule({
   declarations: [
@@ -34,6 +39,7 @@ import { LeaderService } from "./services/leader.service";
     HomeComponent,
     AboutComponent,
     ContactComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -41,12 +47,21 @@ import { LeaderService } from "./services/leader.service";
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatListModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatSlideToggleModule,
   ],
   providers: [DishService, PromotionService, LeaderService],
+  entryComponents: [LoginComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
